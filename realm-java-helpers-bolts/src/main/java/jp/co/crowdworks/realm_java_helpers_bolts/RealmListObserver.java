260f33b2eea6d7057499047f67c9d0d7433404c8
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-12-27 13:25:23 +0900

    Good Bye! Rx

---
+++ realm-java-helpers-bolts/src/main/java/jp/co/crowdworks/realm_java_helpers_bolts/RealmListObserver.java
@@ -0,0 +1,30 @@
+package jp.co.crowdworks.realm_java_helpers_bolts;
+
+import java.util.List;
+
+import io.realm.RealmChangeListener;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
+public abstract class RealmListObserver<T extends RealmObject> extends AbstractRealmResultsObserver<T> {
+    private static final String TAG = RealmListObserver.class.getSimpleName();
+
+    private String previousResultsString;
+
+    @Override
+    protected RealmChangeListener<RealmResults<T>> getListener() {
+        return new RealmChangeListener<RealmResults<T>>() {
+            @Override
+            public void onChange(RealmResults<T> results) {
+                String currentResultString = results != null ? results.toString() : null;
+                if (previousResultsString != null && previousResultsString.equals(currentResultString)) {
+                    return;
+                }
+                previousResultsString = currentResultString;
+                onCollectionChanged(results);
+            }
+        };
+    }
+
+    protected abstract void onCollectionChanged(List<T> models);
+}
---
+++ realm-java-helpers-bolts/src/main/java/jp/co/crowdworks/realm_java_helpers_bolts/RealmObjectObserver.java
@@ -0,0 +1,41 @@
+package jp.co.crowdworks.realm_java_helpers_bolts;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmObject;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+
+public abstract class RealmObjectObserver<T extends RealmObject> extends AbstractRealmResultsObserver<T> {
+    private static final String TAG = RealmObjectObserver.class.getSimpleName();
+
+    private String previousResultString;
+
+    @Override
+    protected final RealmResults<T> queryItems(Realm realm) {
+        return query(realm).findAll();
+    }
+
+    @Override
+    protected final RealmChangeListener<RealmResults<T>> getListener() {
+        return new RealmChangeListener<RealmResults<T>>() {
+            @Override
+            public void onChange(RealmResults<T> results) {
+                T currentResult = extractObjectFromResults(results);
+                String currentResultString = currentResult != null ? currentResult.toString() : null;
+                if (previousResultString != null && previousResultString.equals(currentResultString)) {
+                    return;
+                }
+                previousResultString = currentResultString;
+                RealmObjectObserver.this.onChange(currentResult != null ? realm.copyFromRealm(currentResult) : null);
+            }
+        };
+    }
+
+    protected abstract RealmQuery<T> query(Realm realm);
+    protected abstract void onChange(T model);
+
+    protected T extractObjectFromResults(RealmResults<T> results) {
+        return results.last(null);
+    }
+}
